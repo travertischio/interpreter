465d7031011a7b89f2cd0b94d9c1e95e645821b8
travertischio <traver@capsule8.com>
2017-11-12 13:44:56 -0500

    Add parsing support for return statements

--- ast/ast.go
+++ ast/ast.go
@@ -37,6 +37,14 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
